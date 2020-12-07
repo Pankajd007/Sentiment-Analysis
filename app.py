ad62a241b279068acb149efe681a4c71a33f1dae
Pankaj <57318222+Pankajd007@users.noreply.github.com>
2020-12-07 18:54:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template,jsonify
 from spacy.lang.en.stop_words import STOP_WORDS
 import pickle
-
+from flasgger.utils import  swag_from
+from flasgger import Swagger
+import os
+from flask import Blueprint
+import pathlib
 
 app = Flask(__name__)
+swagger_config_dir = str(pathlib.Path(__file__).resolve().parent.parent)
 
+swagger = Swagger(app)
 @app.route('/')
 def my_form():
     return render_template('form.html')
 
 @app.route('/', methods=['POST'])
+@swag_from(os.path.join(swagger_config_dir, 'swagger_configs', 'swagger_config1.yml'))
+
 def my_form_post():
     stop_words = list(STOP_WORDS)
     text1 = request.form['text1'].lower()
